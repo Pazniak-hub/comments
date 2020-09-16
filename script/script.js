5dd99415cc89a07559a8b9ec8d45a709663160bf
Leo <leo.poznjak@gmail.com>
2020-09-16 13:43:48 +0300

    second commit

--- script/script.js
+++ script/script.js
@@ -9,41 +9,42 @@ const comments = [
 	{ login: 'Garry', comment: 'Ахахаха' },
 	{ login: 'Dovlatov', comment: 'И был таков' },
 	{ login: 'Sasha', comment: 'Ашчушчэния не те' },
-];
-
+],
+showncomments = [],
+commentsToBeShown = 3;//Количество показываемых комментариев
+ //Второй массив
 /**
  * Функция, которая прогоняет элементы массива
  */
 function addComments () {
-	let showncomments = []; //Второй массив
-
-	for (let i = 0; i <= comments.length + 1; i++) {
+	while (showncomments.length !== commentsToBeShown){
 		let b = Math.floor(Math.random() * comments.length);//Великий рандомный идентификатор
-		if (showncomments.includes(comments[b]) || showncomments.length >= 3) {//Элементы не должны повторяться, не более 3-х комментариев на странице
-			continue;
 
-		}
-		else {
+		if (!showncomments.includes(comments[b])) {//Элементы не должны повторяться
 			showncomments.push(comments[b]);//Добавляем уникальный элемент
-			render(showncomments, showncomments.length - 1);//Размещаем на странице
+			
 		}
 	}
-}
-
+	render();//Размещаем на странице
+	} 
 /**
  * Функция, которая "отрисовывает комментарии"
  * @param {array} arr
  * @param {string} key
  */
-function render (arr, key) {
+function render () {
 	const newComment = document.createElement('div');
-
 	//Вставляем элемент в файл
+	for (let key = 0; key < showncomments.length; key++) {
 	newComment.innerHTML += `
-            <h1 class="menu__item-subtitle">${arr[key].login}</h1>
-            <div class="menu__item-descr">${arr[key].comment}</div>
-            `;
-
+            <h1 class="menu__item-subtitle">${showncomments[key].login}</h1>
+            <div class="menu__item-descr">${showncomments[key].comment}</div>
+			`;	
 	//Добавляем в конец таблицы
+	}
 	document.body.append(newComment);
 }
+
+
+
+
