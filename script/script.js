3c8f39d5162de34461e02799642b2ce8ab3ccf6e
Leo <leo.poznjak@gmail.com>
2020-09-17 17:44:26 +0300

    third commit

--- script/script.js
+++ script/script.js
@@ -10,38 +10,67 @@ const comments = [
 	{ login: 'Dovlatov', comment: 'И был таков' },
 	{ login: 'Sasha', comment: 'Ашчушчэния не те' },
 ],
-showncomments = [],
-commentsToBeShown = 3;//Количество показываемых комментариев
- //Второй массив
+showncomments = [], //Второй массив
+newComment = document.createElement('div'),
+commentsToBeShown = 3,//Количество показываемых комментариев
+form = document.querySelector('form'),//Форма
+loginField = document.getElementById('text'),//Поле логина
+commentField = document.getElementById('text_comment');//Поле комментария
+
+
+
 /**
  * Функция, которая прогоняет элементы массива
  */
 function addComments () {
 	while (showncomments.length !== commentsToBeShown){
 		let b = Math.floor(Math.random() * comments.length);//Великий рандомный идентификатор
-
+		
 		if (!showncomments.includes(comments[b])) {//Элементы не должны повторяться
 			showncomments.push(comments[b]);//Добавляем уникальный элемент
-			
-		}
+	}
 	}
 	render();//Размещаем на странице
-	} 
+}
 /**
  * Функция, которая "отрисовывает комментарии"
  * @param {array} arr
  * @param {string} key
  */
-function render () {
-	const newComment = document.createElement('div');
-	//Вставляем элемент в файл
+
+
+function render () {//Вставляем элемент в файл
 	for (let key = 0; key < showncomments.length; key++) {
 	newComment.innerHTML += `
             <h1 class="menu__item-subtitle">${showncomments[key].login}</h1>
-            <div class="menu__item-descr">${showncomments[key].comment}</div>
+			<div class="menu__item-descr">${showncomments[key].comment}</div>
 			`;	
-	//Добавляем в конец таблицы
-	}
-	document.body.append(newComment);
-    }
 
+	}
+	document.body.insertBefore(newComment, form);//Добавляем пере формой
+}
+
+function updelete() {//Удаляем старые комментарии
+	var logPage = document.getElementsByClassName('menu__item-subtitle'),
+		comPage = document.getElementsByClassName('menu__item-descr');
+
+	while(logPage[0] && comPage[0]) {
+		logPage[0].remove();
+		comPage[0].remove();
+	}
+}
+
+form.addEventListener('submit', (e) => {//Обрабатываем форму
+	e.preventDefault();
+
+	let loginText = loginField.value,
+		commentText = commentField.value;
+	
+	showncomments.length = 0;
+	showncomments.push({login:`${loginText}`, comment:`${commentText}`});
+	form.reset();
+	
+	updelete();
+	addComments();
+	
+});	
